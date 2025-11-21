1e9c2a5afcfb164e3e41676c9b7a944d0513bd04
JonasN28 <jonas.novak.sea@gmail.com>
2025-11-21 09:42:47 -0800

    A little bit of off-camera mining
    I did some off-camera mining and have a stack of diamond blocks

--- StringAlgorithms.java
+++ StringAlgorithms.java
@@ -7,7 +7,12 @@ public class StringAlgorithms {
      */
     public static String printCharacters(String word) {
         // TODO: Implement this method
-        return "Shiggle";
+        int x = word.length();
+        while (x > 0){
+           word = word.substring(0, x -1) + "\n" + word.substring(x -1);
+           x --;
+        }
+        return word;
     }
 
 
@@ -18,7 +23,13 @@ public class StringAlgorithms {
      */
     public static String reverseWord(String word) {
         // TODO: Implement this method
-        return "";
+        String backword = "";
+        int x = word.length();
+        while (x > 0){
+           backword += word.substring(x - 1, x);
+           x --;
+        }
+        return backword;
     }
 
 
@@ -29,7 +40,14 @@ public class StringAlgorithms {
      */
     public static String capitalizeString(String sentence) {
         // TODO: Implement this method
-        return "";
+        String capletter = sentence.substring(0,1);
+        String newSentence = capletter.toUpperCase() + sentence.substring(1);
+        while (sentence.indexOf(" ") != -1 ){
+           capletter = sentence.substring(sentence.indexOf(" ") + 1, sentence.indexOf(" ") + 2);
+           newSentence = newSentence.substring(0 , sentence.indexOf(" ") + 1) + capletter.toUpperCase() + sentence.substring(sentence.indexOf(" ") + 2);
+           sentence = sentence.substring(0 , sentence.indexOf(" ")) + "*" + sentence.substring(sentence.indexOf(" ") + 1);
+        }
+        return newSentence;
     }
 
 
@@ -40,7 +58,8 @@ public class StringAlgorithms {
      */
     public static boolean detectPalindrome(String word) {
         // TODO: Implement this method
-        return false;
+        String backWord = reverseWord(word);
+        return (word.toLowerCase().equals(backWord.toLowerCase()));
     }
 
 
@@ -50,9 +69,26 @@ public class StringAlgorithms {
      * first character that appears exactly once. If no such
      * character exists, return a space ' '.
      */
-    public static char firstUniqueChar(String word) {
+    public static String firstUniqueChar(String word) {
         // TODO: Implement this method
-        return ' ';
+        String test = "";
+        int x = 1;
+        int y = 1;
+        while( x <= word.length()){
+           test = word.substring(x - 1,x);
+           while(y <= word.length()){
+              if(test.equals(word.substring(y - 1, y))){
+                 x++;
+                 y = word.length() + 1;
+              } else if(y == word.length()){
+                 return test;
+              }
+              y++;
+           }
+           
+        }
+                
+        return " ";
     }
 
 
@@ -66,22 +102,22 @@ public class StringAlgorithms {
         
         System.out.println("Testing printCharacters:");
         // Example:
-        // System.out.println(printCharacters("hello"));
+         System.out.println(printCharacters("Shiggle"));
 
         System.out.println("\nTesting reverseWord:");
         // Example:
-        // System.out.println(reverseWord("hello"));
+         System.out.println(reverseWord("ShLoGglE"));
 
         System.out.println("\nTesting capitalizeString:");
         // Example:
-        // System.out.println(capitalizeString("the quick brown fox"));
+        System.out.println(capitalizeString("shrimple dimple doodle froodle"));
 
         System.out.println("\nTesting detectPalindrome:");
         // Example:
-        // System.out.println(detectPalindrome("racecar"));
+        System.out.println(detectPalindrome("TAcocat"));
 
         System.out.println("\nTesting firstUniqueChar:");
         // Example:
-        // System.out.println(firstUniqueChar("swiss"));
+        System.out.println(firstUniqueChar("swiss"));
     }
 }
